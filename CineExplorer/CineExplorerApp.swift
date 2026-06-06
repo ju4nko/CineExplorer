d17c01a6200c97695323d2250408c1eb023097f3
ju4nko <ju4nko@gmail.com>
2026-06-07 00:51:47 +0200

    Initial Commit

---
+++ CineExplorer/CineExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineExplorerApp.swift
+//  CineExplorer
+//
+//  Created by Juanjo on 07/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CineExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
